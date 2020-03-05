decc8a48ad06c9c21b12b0f5570c2da2268a0336
Horizon <zhong_jh4848@163.com>
2020-03-05 18:21:51 +0800

    Define Flink and JanusGraph Env .

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Fink
 
 Maven + Scala + FLink Project
+
+# Flink + JanusGraph
---
+++ src/main/scala/cn/ark/cell/stream/StreamInputApp.scala
@@ -0,0 +1,11 @@
+package cn.ark.cell.stream
+
+/**
+  * @Author G.Goe
+  * @Date 2020/3/5
+  * @Required JDK1.8及以上版本 
+  * @Resources
+  */
+object StreamInputApp {
+
+}
